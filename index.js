b2fa761100634bc066f5d7f3fe4366773633f21f
mavasquez401 <mavasquez401@gmail.com>
2024-05-14 23:10:08 -0400

    completed mysql connection

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-// const dotenv = require('dotenv')
-require('dotenv').config()
-const doxname = process.env.SECRET_USER
-const PORT = process.env.PORT
+require('dotenv').config();
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+const doxname = process.env.SECRET_USER;
 console.log(doxname);
 
-const express = require('express')
-const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.send('Hello Geeks!')
-})
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-app.listen(PORT)
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the MySQL database as id ' + connection.threadId);
+});
+
+connection.query('DESCRIBE ' + process.env.DB_TABLE_NAME, (err, results) => {
+  if (err) throw err;
+  console.log(results);
+});
+
+const newUser = {
+  FirstName: 'John',
+  LastName: 'Doe',
+  Department: 'Engineering',
+  StartDate: '2023-01-01',
+  EndDate: null,  // If there's no end date, use null
+  Salary: 75000.00,
+  JobTitle: 'Software Engineer'
+};
+
+const query = `INSERT INTO ${process.env.DB_TABLE_NAME} (FirstName, LastName, Department, StartDate, EndDate, Salary, JobTitle) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+connection.query(query, [newUser.FirstName, newUser.LastName, newUser.Department, newUser.StartDate, newUser.EndDate, newUser.Salary, newUser.JobTitle], (err, results) => {
+  if (err) throw err;
+  console.log('New user added:', results.insertId);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Geeks!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
